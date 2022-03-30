3abde6c13717ea90a35351e49270b0370960e908
Administrator <test@test.ro>
2022-03-30 11:37:41 +0300

    added customer address and contact

--- src/main/java/com/example/finalprojectapp/model/Customer.java
+++ src/main/java/com/example/finalprojectapp/model/Customer.java
@@ -1,6 +1,5 @@
 package com.example.finalprojectapp.model;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
