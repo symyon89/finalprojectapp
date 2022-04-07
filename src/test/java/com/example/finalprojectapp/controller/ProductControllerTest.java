4573613198eba33963e27bfa038103dca0b92c7e
Administrator <test@test.ro>
2022-04-07 10:37:17 +0300

    text corrections

--- src/test/java/com/example/finalprojectapp/controller/ProductControllerTest.java
+++ src/test/java/com/example/finalprojectapp/controller/ProductControllerTest.java
@@ -19,7 +19,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
